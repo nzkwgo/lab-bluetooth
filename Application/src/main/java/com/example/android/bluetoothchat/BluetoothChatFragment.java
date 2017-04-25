55c5aee019897c096f2ada5a417dc4218b71acae
Noah Ezekwugo <nzkwgo@uw.edu>
2017-04-25 16:24:03 -0700

    worked on the lab

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatFragment.java
@@ -92,12 +92,17 @@ public class BluetoothChatFragment extends Fragment {
         setHasOptionsMenu(true);
 
         // TODO: Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         /**
          *  TODO: If the adapter is null, then Bluetooth is not supported.
          *  TODO: Let us know via Toast Bluetooth is not available.
          *  TODO: Then finish() the fragment's attached Activity.
          */
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(getContext(), "Bluetooth isn't available", Toast.LENGTH_SHORT).show();
+            getActivity().finish();
+        }
     }
 
 
@@ -114,6 +119,8 @@ public class BluetoothChatFragment extends Fragment {
                  * TODO: If adapter is NOT enabled, send an Implicit Intent with the action ACTION_REQUEST_ENABLE (send for a result)
                  * TODO: Otherwise, check if the BluetoothChatService is null, and if so call setupChat() to set it up.
                  */
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
 
             }
             else if(mChatService == null){
@@ -203,6 +210,10 @@ public class BluetoothChatFragment extends Fragment {
          * TODO: Check if the adapter is in SCAN_MODE_CONNECTABLE_DISCOVERBLE mode
          * TODO: If not, send an Implicit Intent for the ACTION_REQUEST_DISCOVERABLE action (with an extra for the EXTRA_DISCOVERABLE_DURATION)
          */
+        if (mBluetoothAdapter.getScanMode() != BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        }
 
     }
 
